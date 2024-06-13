c0db497d36ec4ee83d748bcd01641edad2540603
SudeerK <sudeercc.23@gmail.com>
2024-06-13 20:40:10 +0530

    print program

--- 01-helloWorld.sh
+++ 01-helloWorld.sh
@@ -1,1 +1,4 @@
-This is my first program. Let me run a single command to push the code.
+#!/bin/bash
+#Above is called shebang. To check the syntax of shell script we need to give location of bash. 
+#That is given as part of first line in the script. shell script = bash script
+echo "Hello World"
