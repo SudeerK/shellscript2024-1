177d71b47ccc4d4be70cc227162b81101dc5c58c
SudeerK <sudeercc.23@gmail.com>
2024-06-14 14:27:44 +0530

    without displaying value while typing

--- 05-variables.sh
+++ 05-variables.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-echo "Enter UserName: "
+echo "Enter UserName::"
 read -s UserName
-echo "Enter Password: "
+echo "Enter Password::"
 read -s Password
 
-echo "UserName is: $UserName and Password is: $Password"
+echo "UserName is: $UserName and Password is: $Password;"
