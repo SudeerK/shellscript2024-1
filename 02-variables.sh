24b25ccabf28b9aa9343838189ebebc15c94e15d
SudeerK <sudeercc.23@gmail.com>
2024-06-14 11:47:18 +0530

    2nd program

--- 02-variables.sh
+++ 02-variables.sh
@@ -2,5 +2,5 @@
 Person1:Ramesh
 Person2:Suresh
 
-$Person1: Hi $Person2, How are You?
-$Person2: Hi $Person1, I am Fine. How about you?
+echo "$Person1: Hi $Person2, How are You?"
+echo "$Person2: Hi $Person1, I am Fine. How about you?"
