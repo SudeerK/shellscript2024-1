e7d37b63b606fd7aba51025bf36b8d765cc0c129
SudeerK <sudeercc.23@gmail.com>
2024-06-14 12:33:15 +0530

    passing args

--- 04-variables.sh
+++ 04-variables.sh
@@ -3,6 +3,6 @@
 PERSON1=$1
 PERSON2=$2
 
-echo "$Person1:Hi $Person2, How are you?"
-echo "$Person2: I am fine. How are you $Person1?"
+echo "$PERSON1:Hi $PERSON2, How are you?"
+echo "$PERSON2: I am fine. How are you $PERSON1?"
 echo "$Person1: I am good."
