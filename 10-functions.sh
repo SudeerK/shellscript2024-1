a8f6829c50d5cef462fe37ad79b700744da9a799
SudeerK <sudeercc.23@gmail.com>
2024-06-15 16:42:43 +0530

    functions

--- 10-functions.sh
+++ 10-functions.sh
@@ -17,7 +17,7 @@ if [ $ID -ne 0 ]
 then 
 echo "ERROR:: NOT A ROOT USER. LOGIN WITH ROOT USER TO INSTALL SOFTWARE"
 else
-echo "Installation of software is success"
+echo "SUCCESS:: ROOT USER"
 fi
 
 yum install mysql123 -y
