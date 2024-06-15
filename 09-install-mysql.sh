a0f02650e242ee2fb6e4b3cfcc4c346ce5d5496a
SudeerK <sudeercc.23@gmail.com>
2024-06-15 07:26:34 +0530

    instal my sql

--- 09-install-mysql.sh
+++ 09-install-mysql.sh
@@ -14,9 +14,16 @@ ID=$(id -u)
 if [ $ID -ne 0 ]
 then
 echo "Error:: Please run this script with root user."
-exit 1
+exit 1 #Any value other than 0 can be given so that it terminates here
 else
 echo " You are a Root user"
 fi #reverse of if indicating end.
 
-yum install mysql -y
+yum install mysql -y 
+
+if [ $? ne 0 ]
+then 
+echo "ERROR:: Installation failed"
+else 
+echo " Installation Success"
+fi
