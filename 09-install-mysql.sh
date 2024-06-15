e63697ef8c24ba6ea4e672faef58020548294449
SudeerK <sudeercc.23@gmail.com>
2024-06-15 06:58:56 +0530

    instal my sql

--- 09-install-mysql.sh
+++ 09-install-mysql.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+<<comment
 ID=$(id -u)
 echo "Logged in ID is $ID"
 if [ $ID -ne 0 ]
@@ -8,3 +8,12 @@ echo "ERROR::Not a root user, login with root user to continue"
 else
 echo "You are logged in as root user. Continue with your installation"
 fi
+comment
+
+ID=$(id -u)
+if [ $ID -ne 0 ]
+then
+echo "Error:: Please run this script with root user."
+else
+echo " You are a Root user"
+fi #reverse of if indicating end.
