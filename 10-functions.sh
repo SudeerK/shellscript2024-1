3572ee9ae6da08882a0c8b93d24b2d9022a87835
SudeerK <sudeercc.23@gmail.com>
2024-06-15 16:41:15 +0530

    functions

--- 10-functions.sh
+++ 10-functions.sh
@@ -4,19 +4,21 @@ ID=$(id -u)
 echo "Value of ID = $ID"
 VALIDATE()
 {
-if [ $ID -ne 0 ]
-then 
-echo "ERROR:: NOT A ROOT USER. LOGIN WITH ROOT USER TO INSTALL SOFTWARE"
-else
-echo "Installation of software is success"
-fi
-}
-
-yum install mysql123 -y
-VALIDATE()
 if [ $? -ne 0 ]
 then 
 echo "ERROR::INSTALLATION FAILED"
 else
 echo "INSTALLATION SUCCESSFUL"
 fi
+}
+
+
+if [ $ID -ne 0 ]
+then 
+echo "ERROR:: NOT A ROOT USER. LOGIN WITH ROOT USER TO INSTALL SOFTWARE"
+else
+echo "Installation of software is success"
+fi
+
+yum install mysql123 -y
+VALIDATE()
