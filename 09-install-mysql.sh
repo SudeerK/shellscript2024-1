2fb1ebd1dedfd6ce6aa6bd6d37438cd492310587
SudeerK <sudeercc.23@gmail.com>
2024-06-15 06:43:23 +0530

    instal my sql

--- 09-install-mysql.sh
+++ 09-install-mysql.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
-yum install mysql -y
+ID=$(id)
+if [ $ID -ne 0 ]
+then
+echo "Not a root user, login with root user to continue"
+else
+echo "You are logged in as root user. Continue with your installation"
