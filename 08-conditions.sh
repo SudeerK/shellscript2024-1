f78cd83a6d0a5816f5de6951bfbc8a7cdde88748
SudeerK <sudeercc.23@gmail.com>
2024-06-15 06:25:14 +0530

    conditions

--- 08-conditions.sh
+++ 08-conditions.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
 Number=$1
-
-if (($Number>100))
+<<comment
+if (($Number > 100))
 then
 echo "entered value $Number is greater than 100"
 else 
 echo " entered value $Number is less than 100"
 fi
+comment
