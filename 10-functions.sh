64143abbfa49a869eb4f96ce33745f561838d9c3
SudeerK <sudeercc.23@gmail.com>
2024-06-15 16:44:46 +0530

    functions

--- 10-functions.sh
+++ 10-functions.sh
@@ -2,6 +2,7 @@
 
 ID=$(id -u)
 echo "Value of ID = $ID"
+
 VALIDATE()
 {
 if [ $? -ne 0 ]
@@ -17,6 +18,7 @@ fi
 if [ $ID -ne 0 ]
 then 
 echo "ERROR:: NOT A ROOT USER. LOGIN WITH ROOT USER TO INSTALL SOFTWARE"
+exit 1
 else
 echo "SUCCESS:: ROOT USER"
 fi
