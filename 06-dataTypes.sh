847cc458815a4bc11adb577c3901670789852189
SudeerK <sudeercc.23@gmail.com>
2024-06-16 11:23:07 +0530

    special vriables

--- 06-dataTypes.sh
+++ 06-dataTypes.sh
@@ -5,3 +5,7 @@ Num2=$2
 SUM=$(($Num1+$Num2)) #why is it required to give this in multiple braces?
 
 echo "Total sum of $Num1 and $Num2 is : $SUM"
+
+echo "Script Name:: $0"
+echo "Number of args passed :: $#"
+echo "Args passed are :: $@"
